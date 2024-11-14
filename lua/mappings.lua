2fd39c297f026769e2501670f9062e02f9fa49a7
Edgars Mjasnikovs <12757881+mjasnikovs@users.noreply.github.com>
2024-11-14 09:48:17 +0200

    search hotkey change

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -13,8 +13,8 @@ map('i', 'jk', '<ESC>')
 map({ 'n' }, '<leader>ff', '<cmd> NvimTreeToggle <CR>', { desc = 'Toggle nvimtree' })
 map({ 'n' }, '<leader>f', '<cmd> NvimTreeFocus <CR>', { desc = 'Focus nvimtree' })
 
-map({ 'n' }, '<leader>ss', '<cmd> Telescope find_files <CR>', { desc = 'Telescope Find files' })
-map({ 'n' }, '<leader>s', '<cmd> Telescope current_buffer_fuzzy_find <CR>', { desc = 'Telescope Find in buffer' })
+map({ 'n' }, '<leader>s', '<cmd> Telescope find_files <CR>', { desc = 'Telescope Find files' })
+map({ 'n' }, '<leader>ss', '<cmd> Telescope current_buffer_fuzzy_find <CR>', { desc = 'Telescope Find in buffer' })
 
 map({ 'n' }, '<leader>tt', function()
 	require('nvchad.term').toggle {
