06f62b2e9c54578671ac32e64e8762d8f68b1f0e
Edgars Mjasnikovs <12757881+mjasnikovs@users.noreply.github.com>
2025-09-18 08:48:47 +0300

    lspconfig migration

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,25 +1,23 @@
 -- load defaults i.e lua_lsp
 require('nvchad.configs.lspconfig').defaults()
 
-local lspconfig = require 'lspconfig'
-
 -- EXAMPLE
 local servers = { 'html', 'cssls', 'ts_ls', 'eslint' }
 local nvlsp = require 'nvchad.configs.lspconfig'
 
 -- lsps with default config
 for _, lsp in ipairs(servers) do
-	lspconfig[lsp].setup {
+	vim.lsp.config(lsp, {
 		on_attach = nvlsp.on_attach,
 		on_init = nvlsp.on_init,
 		capabilities = nvlsp.capabilities,
-	}
+	})
 end
 
-lspconfig.gdscript.setup {
- 	name = "godot",
-    cmd = {"ncat", "127.0.0.1", "6005"}
-}
+vim.lsp.config('gdscript', {
+	name = 'godot',
+	cmd = { 'ncat', '127.0.0.1', '6005' },
+})
 
 -- configuring single server, example: typescript
 -- lspconfig.ts_ls.setup {
